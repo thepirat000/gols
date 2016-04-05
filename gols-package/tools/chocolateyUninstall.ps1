73afd8e6397c2b938cfbc34c7d141a21b41acd2e
Federico Daniel Colombo <thepirat000@hotmail.com>
2016-04-05 18:43:14 -0500

    changed location of setup.exe to a release

--- gols-package/tools/chocolateyUninstall.ps1
+++ gols-package/tools/chocolateyUninstall.ps1
@@ -1,6 +1,6 @@
 $packageName = 'gols'
 $fileType = 'exe'
-$url = 'https://github.com/thepirat000/gols/blob/master/Setup/Setup/Express/SingleImage/DiskImages/DISK1/setup.exe?raw=true'
+$url = 'https://github.com/thepirat000/gols/releases/download/v1.0.0.1/setup.exe'
 $silentArgs = '/x /v"/qn"'
 
 Install-ChocolateyPackage $packageName $fileType $silentArgs $url
